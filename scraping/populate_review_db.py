3c7e20bff902a69fc7836b840f4fe593b507413e
Megan St. Hilaire <mfsthilaire13@gmail.com>
2020-08-28 12:45:26 -0400

    Added code and tests for getting AKA

--- scraping/populate_review_db.py
+++ scraping/populate_review_db.py
@@ -14,7 +14,7 @@ def get_review_links(list_url):
 
     list_page_soup = BeautifulSoup(response.text, "html.parser")
     list_soup = list_page_soup.find(class_="content")
-    rev_link_elems = list_soup.findAll(href=re.compile("viewreview.php"),
+    rev_link_elems = list_soup.find_all(href=re.compile("viewreview.php"),
                                        string=_not_second_opinion)
     review_links = [rev_link.get('href') for rev_link in rev_link_elems]
     return review_links
@@ -30,3 +30,20 @@ def get_anime_title(review_url):
     except AttributeError:
         raise MissingAnimeDetailError("This anime's review is missing a title.")
     return title
+
+
+def _is_aka_tag(tag):
+    return tag.name == "td" and "AKA" in str(tag)
+
+
+def get_anime_AKA(review_url):
+    response = requests.get(review_url)
+    response.raise_for_status()
+
+    review_page_soup = BeautifulSoup(response.text, "html.parser")
+    try:
+        aka_tag = review_page_soup.find(class_="review").find(_is_aka_tag)
+        aka = aka_tag.text[len("AKA: "):]
+    except AttributeError:
+        raise MissingAnimeDetailError("This anime's review is missing alternative names.")
+    return aka
---
+++ tests/unit/scraping/get_anime_AKA_test.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from scraping.populate_review_db import get_anime_AKA
+from scraping.MissingAnimeDetailError import MissingAnimeDetailError
+
+
+class get_anime_AKA_TestCase(unittest.TestCase):
+
+    @patch('requests.get')
+    def test_can_get_AKA(self, mock_get):
+        review_link = "tests/example_pages/review685.html"
+        review_page = open(review_link, "r")
+        mock_get.return_value = MagicMock()
+        mock_get.return_value.text = review_page
+
+        aka = get_anime_AKA(review_link)
+        expected = ".hack//黄昏の腕輪伝説 (hack//Tasogare no Udewa Densetsu), " \
+                   ".hack//Legend of the Twilight Bracelet, .hack//LEGEND, .hack//DUSK"
+        self.assertEqual(expected, aka)
+        mock_get.assert_called_with("tests/example_pages/review685.html")
+
+    @patch('requests.get')
+    def test_title_missing(self, mock_get):
+        review_link = "tests/example_pages/no_aka.html"
+        review_page = open(review_link, "r")
+        mock_get.return_value = MagicMock()
+        mock_get.return_value.text = review_page
+
+        with self.assertRaises(MissingAnimeDetailError):
+            title = get_anime_AKA(review_link)
+            mock_get.assert_called_with("tests/example_pages/no_aka.html")
+
+
+if __name__ == '__main__':
+    unittest.main()
