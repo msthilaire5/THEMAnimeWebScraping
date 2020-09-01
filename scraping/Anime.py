689642e37dfc31bc5cb9fdb2b0690acbf927511a
Megan St. Hilaire <mfsthilaire13@gmail.com>
2020-08-31 23:53:13 -0400

    Removed module-level functions for anime details, Switched Anime's static getters to instance setters

--- scraping/Anime.py
+++ scraping/Anime.py
@@ -6,6 +6,16 @@ from scraping.MissingAnimeDetailError import MissingAnimeDetailError
 class Anime:
 
     def __init__(self, review_url, review_soup):
+        self.anime_id = None
+        self.title = None
+        self.aka = None
+        self.genre = None
+        self.media_type = None
+        self.num_episodes = None
+        self.mins_per_episode = None
+        self.distributor = None
+        self.content_rating = None
+
         title_tag = review_soup.find('h1')
         aka_tag = review_soup.find('b', text=re.compile("AKA"))
         genre_tag = review_soup.find('b', text=re.compile("Genre"))
@@ -13,76 +23,68 @@ class Anime:
         distributor_tag = review_soup.find('b', text=re.compile("Distributor"))
         cr_tag = review_soup.find('b', text=re.compile("Content Rating"))
 
-        self.anime_id = self._get_id(review_url)
-        self.title = self._get_title(title_tag)
-        self.aka = self._get_aka(aka_tag)
-        self.genre = self._get_genre(genre_tag)
-        self.media_type, self.num_episodes, self.mins_per_episode = self._get_length_info(length_tag)
-        self.distributor = self._get_distributor(distributor_tag)
-        self.content_rating = self._get_content_rating(cr_tag)
+        self._set_id(review_url)
+        self._set_title(title_tag)
+        self._set_aka(aka_tag)
+        self._set_genre(genre_tag)
+        self._set_length_info(length_tag)
+        self._set_distributor(distributor_tag)
+        self._set_content_rating(cr_tag)
         self.url = review_url
 
-    @staticmethod
-    def _get_id(review_url):
+    def _set_id(self, review_url):
         id_idx = review_url.find('=') + 1
-        return int(review_url[id_idx:])
+        self.anime_id = int(review_url[id_idx:])
 
-    @staticmethod
-    def _get_title(title_tag):
+    def _set_title(self, title_tag):
         try:
-            return title_tag.text
+            self.title = title_tag.text
         except AttributeError:
             raise MissingAnimeDetailError("This anime's review is missing a title.")
 
-    @staticmethod
-    def _get_aka(aka_tag):
+    def _set_aka(self, aka_tag):
         try:
-            return aka_tag.next_sibling.strip()
+            self.aka = aka_tag.next_sibling.strip()
         except AttributeError:
             raise MissingAnimeDetailError("This anime's review is missing alternative names.")
 
-    @staticmethod
-    def _get_genre(genre_tag):
+    def _set_genre(self, genre_tag):
         try:
-            return genre_tag.next_sibling.strip()
+            self.genre = genre_tag.next_sibling.strip()
         except AttributeError:
             raise MissingAnimeDetailError("This anime's review is missing its genre.")
 
-    @staticmethod
-    def _get_length_info(length_tag):
+    def _set_length_info(self, length_tag):
         try:
             length_details = length_tag.next_sibling.split(",")
-            media_type = length_details[0].strip()
-            num_episodes = 1 if len(length_details) < 3 else int(length_details[1].split()[0])
-            mins_per_episode = int(length_details[-1].split()[0])
-            return media_type, num_episodes, mins_per_episode
+            self.media_type = length_details[0].strip()
+            self.num_episodes = 1 if len(length_details) < 3 else int(length_details[1].split()[0])
+            self.mins_per_episode = int(length_details[-1].split()[0])
         except AttributeError:
             raise MissingAnimeDetailError("This anime's review is missing its length information.")
 
-    @staticmethod
-    def _get_distributor(distributor_tag):
+    def _set_distributor(self, distributor_tag):
         try:
             distributor_link = distributor_tag.find_next_sibling('a')
             if distributor_link:
-                distributor = distributor_link.string
+                self.distributor = distributor_link.string
             else:
                 distributor_details = distributor_tag.next_sibling
                 by_location = distributor_details.find("by")
                 distributor_start = by_location + len("by ")
                 delimiters = "[{}]".format(string.punctuation)
                 distributor = re.split(delimiters, distributor_details[distributor_start:])[0]
-            return distributor
+                self.distributor = distributor
         except AttributeError:
             raise MissingAnimeDetailError("This anime's review is missing its distributor.")
 
-    @staticmethod
-    def _get_content_rating(cr_tag):
+    def _set_content_rating(self, cr_tag):
         try:
             content_rating = cr_tag.next_sibling
             if "(" in content_rating:
                 explanation_start = content_rating.find("(")
                 content_rating = content_rating[:explanation_start]
-            return content_rating.strip()
+            self.content_rating = content_rating.strip()
         except AttributeError:
             raise MissingAnimeDetailError("This anime's review is missing its content rating.")
 
